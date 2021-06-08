88a0c8202a49667fe8b2c50d592671b10475627d
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-06-08 20:51:50 +0530

    fix footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="text-center text-lg-start bg-light text-muted mt-5">
             <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
                 © 2021 Copyright @ 
-                <a className="text-reset fw-bold link" href="https://doondukan.com/" target="_blank">DoonDukan.com</a>
+                <a className="text-reset fw-bold link"  rel="noreferrer" href="https://doondukan.com/" target="_blank">DoonDukan.com</a>
             </div>
         </footer>
     );
