3633c6429c0ef3402788b580a0a0c8b9855bff5e
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-06-13 01:17:15 +0530

    fix: no product found error message

--- src/Components/product/ProductList.js
+++ src/Components/product/ProductList.js
@@ -5,11 +5,13 @@ function ProductList({ products }) {
     return (
         <>
             {
-                products.map(product => {
+                (products.length)
+                ?products.map(product => {
                     return <div className="col-lg-6 col-12 p-0 m-0">
                         <ProductCard product={product} key={product.id} />
                     </div>
                 })
+                :<h4 className="col-12 text-center p-5"> Oops! No products found under this category</h4>
             }
         </>
     );
