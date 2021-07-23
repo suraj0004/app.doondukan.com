d6bc94a8092bbb98354187c12145db8193f2fffb
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-07-24 00:32:02 +0530

    update: integrated delivery module in checkout

--- src/Components/cart/GrandTotal.js
+++ src/Components/cart/GrandTotal.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React from "react";
 import { FaRupeeSign } from "react-icons/fa";
 
-const GrandTotal = ({grand_total}) => {
+const GrandTotal = ({
+  grand_total,
+  deliveryCharges = null,
+  deliveryMedium = null,
+}) => {
+  if (
+    deliveryCharges &&
+    deliveryMedium &&
+    deliveryMedium !== "user-self-collected"
+  ) {
+    deliveryCharges = Number(deliveryCharges);
     return (
+      <div>
         <div className="row p-2 font-weight-bold text-dark">
-        <div className="col-9 p-0 m-0">
-            Grand Total <small className="text-secondary">(Inclusive of all taxes)</small>
+          <div className="col-9 p-0 m-0">
+            Total Order Amount <small className="text-secondary"></small>
+          </div>
+          <div className="col-3 text-right p-0 m-0">
+            <FaRupeeSign size="15" />
+            {grand_total}
+          </div>
         </div>
-        <div className="col-3 text-right p-0 m-0">
-            <FaRupeeSign size="15" />{grand_total}
+        <div className="row p-2 font-weight-bold text-dark">
+          <div className="col-9 p-0 m-0">
+            Delivery Charges <small className="text-secondary"></small>
+          </div>
+          <div className="col-3 text-right p-0 m-0">
+            <FaRupeeSign size="15" />
+            {deliveryCharges}
+          </div>
         </div>
-    </div>
+        <hr />
+        <div className="row p-2 font-weight-bold text-dark">
+          <div className="col-9 p-0 m-0">
+            Grand Total{" "}
+            <small className="text-secondary">(Inclusive of all taxes)</small>
+          </div>
+          <div className="col-3 text-right p-0 m-0">
+            <FaRupeeSign size="15" />
+            {grand_total + deliveryCharges}
+          </div>
+        </div>
+      </div>
     );
+  }
+  return (
+    <div className="row p-2 font-weight-bold text-dark">
+      <div className="col-9 p-0 m-0">
+        Grand Total{" "}
+        <small className="text-secondary">(Inclusive of all taxes)</small>
+      </div>
+      <div className="col-3 text-right p-0 m-0">
+        <FaRupeeSign size="15" />
+        {grand_total}
+      </div>
+    </div>
+  );
 };
 
 export default GrandTotal;
