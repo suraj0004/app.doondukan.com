79890add9828819e70d9f1129ee37bc6343445b1
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-08-10 21:53:56 +0530

    update: showing delivery charges on order detail page

--- src/Components/myAccount/orders/detail/OrderDetail.js
+++ src/Components/myAccount/orders/detail/OrderDetail.js
@@ -1,60 +1,61 @@
-import React from 'react';
+import React from "react";
 import ShopInfo from "Components/common/ShopInfo";
-import OrderTimeline from './OrderTimeline'
-import OrderItem from './OrderItem'
-import GrandTotal from "Components/cart/GrandTotal"
-import BuyerInfo from "./BuyerInfo"
+import OrderTimeline from "./OrderTimeline";
+import OrderItem from "./OrderItem";
+import GrandTotal from "Components/cart/GrandTotal";
+import BuyerInfo from "./BuyerInfo";
 const OrderDetail = ({ data }) => {
-    let grand_total = 0
-    const deliveryInfo = {
-        isHomeDelivery: data.is_home_delivery,
-        deliveryType: data.delivery_type,
-        fromTime: data.from_time,
-        toTime: data.to_time,
-        deliveryAddress: data.delivery_address,
-    }
-    return (
-        <div className="col-12 card">
-            <div className="row">
-                <div className="col-md-6">
-                    <ShopInfo shop_info={data.store} />
-                </div>
-                <div className="col-md-6">
-                    <OrderTimeline status={data.status} order_no={data.order_no} />
-              </div>
-            </div>
-            <div className="row">
-                <div className="col-md-2">
-
-                </div>
-                <div className="col-md-8">
-                    <hr/>
-                    <h4 className="text-center border-bottom border-left border-right pb-4"> 
-                    Order No : #{data.order_no}
-                    <br/> 
-                    Total Items : {data.items.length}
-                    </h4>
-                    {
-                        data.items.map(item =>{
-                            grand_total += (item.quantity * Number(item.price).toFixed(2) )
-                            return  <OrderItem item={item} />
-                        })
-                    }
-                    <hr/>
-                    <div className="pr-3 text-right">
-                    <GrandTotal grand_total={grand_total} />
-                    </div>
-                    <hr/>
-                </div>
-            </div>
-            <div className="row">
-            <div className="col-md-2"></div>
-               <div className="col-md-8">
-               <BuyerInfo buyer={data.buyer} deliveryInfo={deliveryInfo} />
-               </div>
-            </div>
+  let grand_total = 0;
+  const deliveryInfo = {
+    isHomeDelivery: data.is_home_delivery,
+    deliveryType: data.delivery_type,
+    fromTime: data.from_time,
+    toTime: data.to_time,
+    deliveryAddress: data.delivery_address,
+    deliveryCharges: data.delivery_charges,
+  };
+  return (
+    <div className="col-12 card">
+      <div className="row">
+        <div className="col-md-6">
+          <ShopInfo shop_info={data.store} />
         </div>
-    );
+        <div className="col-md-6">
+          <OrderTimeline status={data.status} order_no={data.order_no} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-2"></div>
+        <div className="col-md-8">
+          <hr />
+          <h4 className="text-center border-bottom border-left border-right pb-4">
+            Order No : #{data.order_no}
+            <br />
+            Total Items : {data.items.length}
+          </h4>
+          {data.items.map((item) => {
+            grand_total += item.quantity * Number(item.price).toFixed(2);
+            return <OrderItem item={item} />;
+          })}
+          <hr />
+          <div className="pr-3 text-right">
+            <GrandTotal
+              grand_total={grand_total}
+              deliveryCharges={deliveryInfo.deliveryCharges}
+              isHomeDelivery={deliveryInfo.isHomeDelivery}
+            />
+          </div>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-2"></div>
+        <div className="col-md-8">
+          <BuyerInfo buyer={data.buyer} deliveryInfo={deliveryInfo} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderDetail;
