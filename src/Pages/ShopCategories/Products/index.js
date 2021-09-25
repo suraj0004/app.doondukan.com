4872c7ac90446911e7cc59d6412d88476d2fdade
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-09-25 10:16:50 +0530

    add: product search functionality in category  and product page

--- src/Pages/ShopCategories/Products/index.js
+++ src/Pages/ShopCategories/Products/index.js
@@ -1,20 +1,30 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchProducts } from "ReduxStore/index";
 import ProductList from "Components/product/ProductList";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProductLoader from "Components/product/ProductLoader";
 import MainLayout from "Layouts/Main";
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { DebounceInput } from "react-debounce-input";
 
 function Products({ fetchProducts, products, global }) {
+  const queryParam = new URLSearchParams(useLocation().search);
   const { shop_slug, category_slug } = useParams();
+  const [search, setSearch] = useState(queryParam.get("search"));
 
   useEffect(() => {
     if (shop_slug && category_slug) {
-      fetchProducts(shop_slug, category_slug);
+      fetchProducts(shop_slug, category_slug, search);
     }
   }, [shop_slug, category_slug, fetchProducts]);
+
+  const handleProductSearch = (e) => {
+    const search = e.target.value;
+    setSearch(search);
+    fetchProducts(shop_slug, category_slug, search);
+  };
 
   return (
     <MainLayout>
@@ -28,6 +38,30 @@ function Products({ fetchProducts, products, global }) {
               Products
             </li>
           </ol>
+        </nav>
+        <nav>
+        <div class="shadow p-0 rounded">
+          <label class="sr-only" for="product_search_bar">
+            Search for products
+          </label>
+          <div class="input-group mb-2">
+            <DebounceInput
+              value={search}
+              type="text"
+              class="form-control"
+              id="product_search_bar"
+              placeholder="Search for products"
+              debounceTimeout={1000}
+              onChange={handleProductSearch}
+            />
+
+            <div class="input-group-prepend">
+              <div class="input-group-text">
+                <FaSearch />
+              </div>
+            </div>
+          </div>
+        </div>
         </nav>
         <div className="row">
           {global.loading ? (
@@ -50,8 +84,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchProducts: (shop_slug, category_slug) =>
-      dispatch(fetchProducts(shop_slug, category_slug)),
+    fetchProducts: (shop_slug, category_slug, search) =>
+      dispatch(fetchProducts(shop_slug, category_slug, search)),
   };
 };
 
--- src/Pages/ShopCategories/index.js
+++ src/Pages/ShopCategories/index.js
@@ -1,15 +1,18 @@
-import { connect } from 'react-redux';
-import { fetchCategories, setShopSlug } from 'ReduxStore/index.js'
-import CategoryList from 'Components/category/CategoryList'
-import { useEffect } from 'react';
-import CategoryLoader from 'Components/category/CategoryLoader'
-import MainLayout from 'Layouts/Main'
-import { useParams } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { fetchCategories, setShopSlug, fetchProductSearchResult } from "ReduxStore/index.js";
+import CategoryList from "Components/category/CategoryList";
+import { useEffect, useState } from "react";
+import CategoryLoader from "Components/category/CategoryLoader";
+import MainLayout from "Layouts/Main";
+import { useParams, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AsyncPaginate } from "react-select-async-paginate";
 
 function ShopCategories({ fetchCategories, categories, setShopSlug }) {
 
-  const { shop_slug } = useParams()
+  const [searchProduct, setSearchProduct] = useState(null);
+
+  const { shop_slug } = useParams();
 
   useEffect(() => {
     if (shop_slug) {
@@ -17,38 +20,66 @@ function ShopCategories({ fetchCategories, categories, setShopSlug }) {
       setShopSlug(shop_slug);
     }
   }, [shop_slug, fetchCategories, setShopSlug]);
+
+
+  const history = useHistory();
+  const handleProductClick = (selectedProduct) => {
+    setSearchProduct(selectedProduct);
+    history.push(selectedProduct.value);
+  }
+
   return (
     <MainLayout>
       <div className="page-padding-top">
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
-            <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-            <li className="breadcrumb-item active" aria-current="page">Categories</li>
+            <li className="breadcrumb-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Categories
+            </li>
           </ol>
         </nav>
+        <nav>
+          <div class="breadcrumb p-0 rounded mb-3 ">
+            <AsyncPaginate
+              className="form-control p-0 m-0"
+              value={searchProduct}
+              loadOptions={fetchProductSearchResult}
+              debounceTimeout={500}
+              onChange={handleProductClick}
+              additional={{
+                page: 1,
+                shop_slug: shop_slug
+              }}
+            />
+          </div>
+        </nav>
+
         <div className="row">
-          {
-            categories.loading
-              ? <CategoryLoader />
-              : <CategoryList categories={categories.data} />
-          }
+          {categories.loading ? (
+            <CategoryLoader />
+          ) : (
+            <CategoryList categories={categories.data} />
+          )}
         </div>
       </div>
     </MainLayout>
-  )
+  );
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
     categories: state.categories,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchCategories: (shop_slug) => dispatch(fetchCategories(shop_slug)),
-    setShopSlug: (shop_slug) => dispatch(setShopSlug(shop_slug))
-  }
-}
+    setShopSlug: (shop_slug) => dispatch(setShopSlug(shop_slug)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShopCategories);
