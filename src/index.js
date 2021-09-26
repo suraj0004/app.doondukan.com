e8516a188ef0a5d7912350af16bb0f566d232a68
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-09-27 02:42:35 +0530

    testing app update

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from 'ReduxStore/store';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-// import '@pwabuilder/pwaupdate'
+import '@pwabuilder/pwaupdate';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} >
       <App />
-      {/* <pwa-update></pwa-update> */}
+      <pwa-update></pwa-update>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
@@ -19,20 +20,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
-serviceWorkerRegistration.register({
-  onUpdate: (worker) => {
-    console.log('onUpdate');
-    if(worker?.waiting?.postMessage){
-      console.log('before postMessage');
-
-      worker.waiting.postMessage({ type: 'SKIP_WAITING' });
-      console.log('after postMessage');
-
-      window.location.reload(true);
-    }
-  },
-});
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
