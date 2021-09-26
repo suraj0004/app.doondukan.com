655779a07d8dbffd8f7e5ee807a34a8eb2e902b2
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-09-27 02:33:09 +0530

    testing app update

--- src/index.js
+++ src/index.js
@@ -19,16 +19,18 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
 serviceWorkerRegistration.register({
-  onUpdate: (e) => {
-    console.log("on update suraj",e);
-    const { waiting: { postMessage = null } = {}, update } = e || {};
-    if (postMessage) {
-      postMessage({ type: 'SKIP_WAITING' });
-    }
-    update().then(() => {
+  onUpdate: (worker) => {
+    console.log('onUpdate');
+    if(worker?.waiting?.postMessage){
+      console.log('before postMessage');
+
+      worker.waiting.postMessage({ type: 'SKIP_WAITING' });
+      console.log('after postMessage');
+
       window.location.reload(true);
-    });
+    }
   },
 });
 // If you want to start measuring performance in your app, pass a function
